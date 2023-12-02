1b5db8cd77b606f48faaa26b965ff17e232ec2d9
jal1999 <58450211+jal1999@users.noreply.github.com>
2023-12-02 17:58:38 -0500

    Delete main.cpp

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "include/util.h"
-
-int main() {
-    std::vector<const char*> vec{"h","i"};
-    std::cout << join<std::vector<const char*>, const char*>(vec, "\n");
-    return 0;
-}
