6662986ec245b2dcbe7baf563f7b185bf7e70026
James <jlafarr99@gmail.com>
2023-12-03 12:44:59 -0500

    Added executor implementation

---
+++ include/executor.h
@@ -0,0 +1,29 @@
+#ifndef UTIL_EXECUTOR_H
+#define UTIL_EXECUTOR_H
+
+#include <future>
+#include <thread>
+
+namespace util {
+    class Executor {
+    public:
+        Executor() = default;
+
+        template <typename ReturnType, typename Callable, typename... Args>
+        requires std::invocable<Callable, Args...>
+        std::future<ReturnType> submit(Callable task, Args... args)
+        {
+            return std::async(task, args...);
+        }
+
+        template <typename Callable, typename... Args>
+        requires std::invocable<Callable, Args...>
+        static void execute(Callable task, Args... args)
+        {
+            auto t = std::jthread{task, args...};
+        }
+    private:
+    };
+}
+
+#endif
